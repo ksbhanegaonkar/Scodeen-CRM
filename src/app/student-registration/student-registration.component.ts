42a409d08feaecc1bf315fe8ff869a49ee788d18
Kedar Bhanegaonkar <ksbhanegaonkar@gmail.com>
2020-01-11 12:21:15 +0530

    Added batch dropdown in registration page

--- src/app/student-registration/student-registration.component.ts
+++ src/app/student-registration/student-registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CandidateService } from '../candidate-details/candidate-details-service.service';
 
 @Component({
   selector: 'app-student-registration',
@@ -6,10 +7,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-registration.component.css']
 })
 export class StudentRegistrationComponent implements OnInit {
-
-  constructor() { }
+  batchDropdownList = [];
+  selectedItems = [];
+  dropdownSettings = {};
+  constructor(private candidateService : CandidateService) { }
 
   ngOnInit() {
+
+
+
+  // this.batchDropdownList = [
+  //   { item_id: 1, item_text: 'Mumbai' },
+  //   { item_id: 2, item_text: 'Bangaluru' },
+  //   { item_id: 3, item_text: 'Pune' },
+  //   { item_id: 4, item_text: 'Navsari' },
+  //   { item_id: 5, item_text: 'New Delhi' }
+  // ];
+  // this.selectedItems = [
+  //   { item_id: 3, item_text: 'Pune' },
+  //   { item_id: 4, item_text: 'Navsari' }
+  // ];
+  this.candidateService.getBatchList().subscribe(
+    (l)=>{
+      this.batchDropdownList = l;
+    }
+  );
+  this.dropdownSettings = {
+    singleSelection: false,
+    idField: 'item_id',
+    textField: 'item_text',
+    selectAllText: 'Select All',
+    unSelectAllText: 'UnSelect All',
+    itemsShowLimit: 10,
+    allowSearchFilter: true
+  };
+  }
+
+
+  onItemSelect(item: any) {
+    console.log(item);
+  }
+  onSelectAll(items: any) {
+    console.log(items);
   }
 
 }
